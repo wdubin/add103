923d925d4e1b2c6383635c8d21ed828dcd174b3c
William Dubin <wdubin@students.mchenry.edu>
2024-10-30 16:57:13 -0500

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,25 @@
 /* change the image */
-function change(picture) {
-    document.getElementById("large").src = picture;
+function change01(picture) {
+    document.getElementById("large").src = "images/dog-8448345_640.jpg";
+  }
+  
+  /* change the image */
+function change02(picture) {
+    document.getElementById("large").src = "images/bird-7073243_640.jpg";
+  }
+  
+  /* change the image */
+function change03(picture) {
+    document.getElementById("large").src = "images/sea-2361247_640.jpg";
+  }
+  
+  /* change the image */
+function change04(picture) {
+    document.getElementById("large").src = "images/snail-7536861_640.jpg";
+  }
+  
+  /* change the image */
+function change05(picture) {
+    document.getElementById("large").src = "images/ladybug-8015502_640.jpg";
   }
   
